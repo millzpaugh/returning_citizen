949b92efab9bd71b96cbbd3075af1dea7ada3e8f
Ann Millspaugh <ann.millspaugh@gmail.com>
2016-03-14 18:26:24 -0400

    add clean resource data

---
+++ app/management/commands/clean_resource_data.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from app.management.scraping.get_data import reader_all
+from app.models import Provider, Resource, Location
+
+def clean_location_data(location):
+    location.phone = location.phone.replace('(u\'', '').replace('\',)','')
+    location.hours_open = location.hours_open.replace('(u\'', '').replace('\',)','')
+    location.population = location.population.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
+    location.save()
+
+def clean_provider_data(provider):
+    provider.population_served = provider.population_served.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
+    provider.programs = provider.programs.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
+    provider.language_services = provider.language_services.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
+    provider.services = provider.services.replace('([u\'', '').replace('\'],)','').replace('\'u','').replace('u\'','').replace('\'','')
+    provider.save()
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Location.objects.destroy_all()
+        # Provider.objects.destroy_all()
+        # Resource.objects.destroy_all()
+
+        providers = Provider.objects.all()
+        for p in providers:
+            clean_provider_data(p)
+
+            if Location.objects.filter(provider=p).exists():
+                location = Location.objects.filter(provider=p)[0]
+                clean_location_data(location)
+
+
+
+
--- app/management/commands/populate_db.py
+++ app/management/commands/populate_db.py
@@ -2,19 +2,6 @@ from django.core.management.base import BaseCommand
 from app.management.scraping.get_data import reader_all
 from app.models import Provider, Resource, Location
 
-def clean_location_data(location):
-    location.phone = location.phone.replace('(u\'', '').replace('\',)','')
-    location.hours_open = location.hours_open.replace('(u\'', '').replace('\',)','')
-    location.population = location.population.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
-    location.save()
-
-def clean_provider_data(provider):
-    provider.population_served = provider.population_served.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
-    provider.programs = provider.programs.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
-    provider.language_services = provider.language_services.replace('[u\'', '').replace('\']','').replace('\'u','').replace('u\'','').replace('\'','')
-    provider.services = provider.services.replace('([u\'', '').replace('\'],)','').replace('\'u','').replace('u\'','').replace('\'','')
-    provider.save()
-
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
